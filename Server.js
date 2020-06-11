325e2a386e4d1feec871d8441ad7649b6ef09c19
Alisson Medeiros <alisilva1@gmail.com>
2020-06-11 13:28:01 -0300

    Inicio do Projeto com Server

---
+++ Server.js
@@ -0,0 +1,22 @@
+const express= require('express')
+const nunjucks= require('nunjucks')
+
+const server= express()
+
+server.use(express.static('public'))
+
+server.set("view engine", "html")
+
+nunjucks.configure("views",{ express:server})
+
+server.get("/", function(req,res){
+    return res.render("about")
+})
+
+server.get("/portfolio", function(req,res){
+    return res.render("portfolio")
+})
+
+server.listen(5000, function(){
+    console.log("server is running")
+})
