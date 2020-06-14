7a300c81b288a0c72fdc85192b2fc92f404835e9
Alisson Medeiros <alisilva1@gmail.com>
2020-06-14 18:27:11 -0300

    Atualizando dados em arquivos externos fora do html atraves do nunjucks

--- Server.js
+++ Server.js
@@ -8,10 +8,24 @@ server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
-nunjucks.configure("views",{ express:server})
+nunjucks.configure("views",{
+     express:server,
+     autoescape: false
+    })
 
 server.get("/", function(req,res){
-    return res.render("about")
+    const header = {
+        avatar_url: "https://avatars3.githubusercontent.com/u/38330004?s=460&u=f5b8b45e98fbbdee21e5da2e023216cb42af8416&v=4",
+        name: "Alisson Medeiros",
+        job: 'Consultor na empresa <a href= "https://www.mobileum.com/" target="_blank"> Mobileum </a>',
+        description: "Curioso em Design e atualmente analista de sistemas",
+        link: [
+            {name: "Github", url: "https://github.com/alisilva1"},
+            {name: "Facebook", url: "https://www.facebook.com/alisson.silva.77920"},
+            {name: "Linkedin", url: "https://www.linkedin.com/in/alisson-medeiros-337450119/"}
+        ]
+    }
+    return res.render("about", {about: header})
 })
 
 server.get("/portfolio", function(req,res){
