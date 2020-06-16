49bc3eda8a73cbde0512a03e6af5398691cec05a
Alisson Medeiros <alisilva1@gmail.com>
2020-06-15 23:58:37 -0300

    finalizado a parte e abrir video em nova aba e retirada do iframe scriptjs

--- public/scripts.js
+++ public/scripts.js
@@ -3,13 +3,7 @@ const cards = document.querySelectorAll('.card')
 
 for(let card of cards){
     card.addEventListener("click",function(){
-        const videoId = card.getAttribute("id")
-        modalOverlay.classList.add('active')
-        modalOverlay.querySelector("iframe").src= `https://youtube.com/embed/${videoId}`
+        const videoId = card.getAttribute("id");
+        window.location.href = `/video?id=${videoId}`
     })
 }
-
-document.querySelector(".close-modal").addEventListener("click",function(){
-    modalOverlay.classList.remove('active')
-    modalOverlay.querySelector("iframe").src = ""
-})
