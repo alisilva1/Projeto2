057101e9c3844598811839ae0df57214f3a4e454
Alisson Medeiros <alisilva1@gmail.com>
2020-06-17 15:12:06 -0300

    Teste pasta app

---
+++ app/server.js
@@ -0,0 +1,51 @@
+const express= require('express')
+const nunjucks= require('nunjucks')
+
+const server= express()
+const videos = require('.app/data')
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views",{
+     express:server,
+     autoescape: false,
+     noCache: true
+    })
+
+server.get("/", function(req,res){
+    const header = {
+        avatar_url: "https://avatars3.githubusercontent.com/u/38330004?s=460&u=f5b8b45e98fbbdee21e5da2e023216cb42af8416&v=4",
+        name: "Alisson Medeiros",
+        job: 'Consultor na empresa <a href= "https://www.mobileum.com/" target="_blank"> Mobileum </a>',
+        description: "Curioso em Design e atualmente analista de sistemas",
+        link: [
+            {name: "Github", url: "https://github.com/alisilva1"},
+            {name: "Facebook", url: "https://www.facebook.com/alisson.silva.77920"},
+            {name: "Linkedin", url: "https://www.linkedin.com/in/alisson-medeiros-337450119/"}
+        ]
+    }
+    return res.render("about", {about: header})
+})
+
+server.get("/portfolio", function(req,res){
+    return res.render("portfolio", { itens: videos})
+})
+
+server.get("/video", function(req,res){
+    const id = req.query.id
+    const video = videos.find(function(video){
+        return video.id == id
+    })
+    if (!video){
+        return res.send("Video not found!")
+    }
+    return res.render("video", { item: video })
+
+})
+
+server.listen(5000, function(){
+    console.log("server is running")
+})
+
