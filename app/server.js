5ec4d0f8f00e2a3326c4cee4c6cd3e0b45dc93d7
Alisson Medeiros <alisilva1@gmail.com>
2020-06-17 15:33:34 -0300

    configuração PORT Error R10 (Boot timeout) -> Web process failed to bind to  within 60 seconds of launch

--- app/server.js
+++ app/server.js
@@ -4,7 +4,13 @@ const nunjucks= require('nunjucks')
 const server= express()
 //const videos = require('app/data')
 
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
+
 server.use(express.static('public'))
+
 
 server.set("view engine", "njk")
 
