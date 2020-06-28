f36ec28962d5fcc246ab5a059d6cf83aaf637d95
Alisson Medeiros <alisilva1@gmail.com>
2020-06-28 18:09:37 -0300

    ajuste descricao

--- app/server.js
+++ app/server.js
@@ -25,8 +25,8 @@ server.get("/", function(req,res){
     const header = {
         avatar_url: "https://avatars3.githubusercontent.com/u/38330004?s=460&u=f5b8b45e98fbbdee21e5da2e023216cb42af8416&v=4",
         name: "Alisson Medeiros",
-        job: 'Consultor na empresa <a href= "https://www.mobileum.com/" target="_blank"> Mobileum </a>',
-        description: "Curioso em Design e atualmente analista de sistemas",
+        job: 'Estagiário na empresa <a href= "https://www.mobileum.com/" target="_blank"> Mobileum </a>',
+        description: "Analista de sistemas",
         link: [
             {name: "Github", url: "https://github.com/alisilva1"},
             {name: "Facebook", url: "https://www.facebook.com/alisson.silva.77920"},
